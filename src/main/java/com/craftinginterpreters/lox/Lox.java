4ee2820a1ce27954cc0133c3db1ef376744f0cec
Yumin Gui <guiyumin@protonmail.com>
2025-04-02 16:19:50 -0700

    feat: interpreter class

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -36,6 +38,8 @@ public class Lox {
 
         if (hadError)
             System.exit(65);
+        if (hadRuntimeError)
+            System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -60,7 +64,8 @@ public class Lox {
         // Stop if there was a syntax error.
         if (hadError)
             return;
-        System.out.println(new AstPrinter().print(expression));
+
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -70,6 +75,11 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
 }
